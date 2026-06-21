d19a97650af08c02b870c4ee1c6a769b4f23ec33
yueyue95 <kertian95@gmail.com>
2026-06-21 21:48:29 +0800

    修复组件滚动与同步地址

--- src/index.ts
+++ src/index.ts
@@ -151,7 +151,7 @@ export class TongduMCP extends McpAgent<Env> {
   );
 
   async init() {
-    const widgetUri = "ui://widget/tongdu-reader-v3.html";
+    const widgetUri = "ui://widget/tongdu-reader-v4.html";
     registerAppResource(
       this.server,
       "tongdu-reader-widget",
